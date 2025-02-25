eaa73ade900b5e9b028c113d9dbac67d56a7904d
Md Abu Musa <musa.fci@gmail.com>
2025-02-25 22:48:56 +0600

    Anonymous function

---
+++ 10_anonymous_function/example_2/main.go
@@ -0,0 +1,19 @@
+/*
+	Passing Anonymous Functions as Arguments
+	Since functions can be passed as arguments, anonymous functions are useful for higher-order functions.
+*/
+
+package main
+
+import "fmt"
+
+func operate(x int, y int, operation func(int, int) int) int {
+	return operation(x, y)
+}
+
+func main() {
+	result := operate(10, 20, func(x int, y int) int {  // this is also called function statement
+		return x + y
+	})
+	fmt.Println(result)
+}
