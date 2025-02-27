1d3cf1f1d93a992b8187bfff0474833cdfd01142
Md Abu Musa <musa.fci@gmail.com>
2025-02-27 09:23:29 +0600

    First order function

---
+++ 13_first_order_function/main.go
@@ -0,0 +1,18 @@
+/*
+	First Order Function:
+	- Standard function or named function
+	- Anonymous function
+	- IIFE function
+	- Function Expression are examples of first order function.
+
+	- They work with primitive data types or structs. like int, string, float, bool, structs etc.
+	- They don't work accept function as an parameter or return a function as its result.	
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
